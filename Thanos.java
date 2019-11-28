816975bc1d7e791957b12e816f69c1c5f2fe6ce9
housseyn007 <55182577+housseyn007@users.noreply.github.com>
2019-11-28 10:08:22 +0100

    Update Thanos.java

--- Thanos.java
+++ Thanos.java
@@ -17,6 +17,8 @@ public class Thanos {
         // TODO 1 : Retrieve and remove the oldest hero in one line
         Hero oldest = heroes.last();
         heroes.remove(oldest);
+        // heroes.pollLast();
+
 
         System.out.println("\nOldest hero:");
         System.out.println(oldest == null ? "" : oldest.getName());
